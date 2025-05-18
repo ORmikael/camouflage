d67f995c55f114a60f80284d564f59203ce38e2d
ormikael <mikaelrandiga@gmail.com>
2025-05-18 09:56:45 +0300

    edited database connection configuration to use our config file that toggles betweenproduction env and development env variables due to TLS issue ( local connection does no require a tls handshake)

--- backend/db.py
+++ backend/db.py
@@ -1,20 +1,54 @@
 
-# ===============================
-# DATABASE CONNECTION MODULE
-# ===============================
+# # ===============================
+# # DATABASE CONNECTION MODULE
+# # ===============================
 
-from pymongo import MongoClient
-from config import PesapalConfig
+
+
 import os
 import certifi
+from pymongo import MongoClient
+from config import PesapalConfig
 os.environ["SSL_CERT_FILE"] = certifi.where()
 
-def get_db():
+
+# ===============================
+# ENVIRONMENT SETUP
+# ===============================
+ENV = os.environ.get("FLASK_ENV", "development").lower()
+
+# ===============================
+# DATABASE CONNECTION
+# ===============================
+def get_db():  
     # USE RESOLVED CONFIG VALUES
     mongo_uri = PesapalConfig.MONGO_URI
     db_name = PesapalConfig.DB_NAME
-
-    client = MongoClient(mongo_uri,tls=True, tlsCAFile=certifi.where())  # Force trusted CA bundle
-    db = client[db_name]
-
+    try:
+        if ENV == "production":
+            print("[DB] Production mode - using TLS connection")
+            client = MongoClient(
+                mongo_uri,
+                tls=True,
+                tlsCAFile=certifi.where()
+            )
+        else:
+            print("[DB] Development mode - using local non-TLS connection")
+            client = MongoClient(mongo_uri)
+    
+        db = client[db_name]
+        booking_collection = db["bookings"]
+    
+        # ===============================
+        # ENSURE UNIQUE BOOKING INDEX
+        # ===============================
+        booking_collection.create_index(
+            [("email", 1), ("date", 1)],
+            unique=True
+        )
+    
+        print(f"[DB] Connected to DB: {db.name}")
+    
+    except Exception as e:
+        print(f"[DB] Connection error: {e}")
     return db
