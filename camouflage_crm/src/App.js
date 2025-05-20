7b85ab4a8e6d22e32df8742f250e219b74734846
ormikael <mikaelrandiga@gmail.com>
2025-05-20 05:14:15 +0300

    modified styling for button for in the aside component

--- camouflage_crm/src/App.js
+++ camouflage_crm/src/App.js
@@ -11,28 +11,52 @@ import ProfilePage from './components/profile';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MediaUploader from './components/media_uploader';
+import AuthPage from './components/authpage';
+
+import ProtectedRoute from './components/protectedroutes';
+import AdminDashboard from './components/admindash';
+import StaffDashboard from './components/staffdash';
+import MyBookings from './components/userProfilecomponets/mybookings';
 
 function App() {
 
   const location = useLocation();
-  const isProfilePage = location.pathname === '/profile'; 
+  const hiddenHeaderPaths = ["/profile", "/login", "/signup"];
+  const isHidden = hiddenHeaderPaths.includes(location.pathname);
+
+  // const isProfilePage = location.pathname === '/profile'; 
   return ( 
   <>
 
-    {!isProfilePage && <Header />}
+    {!isHidden && <Header />}
     <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/destinations" element={<Destinations />} />
           <Route path="/packages" element={<Packages />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          {/* <Route path="/profile" element={<ProfilePage />} /> */}
           <Route path="/uploadmedia" element={<MediaUploader />} />
           {/* {console.log(!isProfilePage)} */}
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/signup" element={<AuthPage />} />
+
+
+          {/* ===================protected routes =================== */}
+
+
+
+          {/* Normal User Route */}
+          <Route path="/profile" element={ <ProtectedRoute allowedRoles={["user", "staff", "management"]}><ProfilePage /></ProtectedRoute>}/>
+          <Route path="/bookings" element={<ProtectedRoute allowedRoles={["user", "staff", "management"]}><MyBookings /></ProtectedRoute>} /> 
+
+          {/* administrative routes  */}
+          <Route path="/staff" element={<ProtectedRoute allowedRoles={["staff", "management"]}><StaffDashboard /></ProtectedRoute>}/>
+          <Route path="/admin"  element={<ProtectedRoute allowedRoles={["management"]}><AdminDashboard /></ProtectedRoute>}/>
 
     </Routes>
 
-    {!isProfilePage && <Footer />}
+    {!isHidden && <Footer />}
   </>
     
   );
